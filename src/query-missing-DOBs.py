6addbf4c5ccbfbd669cfabf6f6cac6d2d0415370
ninpnin <vainoyrjanainen@icloud.com>
2024-02-27 15:55:54 +0100

    fix: cli arguments

--- src/query-missing-DOBs.py
+++ src/query-missing-DOBs.py
@@ -2,9 +2,9 @@
 import pandas as pd
 from wikidata.client import Client
 
-def main():
+def main(args):
 
-	df = pd.read_csv("corpus/quality_assessment/known_mps/catalog.csv", sep=';')
+	df = pd.read_csv(f"{args.qa_folder}/known_mps/catalog.csv", sep=';')
 	#df = pd.read_csv("corpus/quality_assessment/known_mps/integrity-error_missing-birthdate.csv", sep=';')
 	#DOBisNA = df[df['born'].isna()]
 
@@ -21,10 +21,14 @@ def main():
 		else:
 			print("Known DOB")
 
-	df.to_csv("corpus/quality_assessment/known_mps/catalog.csv", sep=';', index=False)
+	df.to_csv(f"{args.qa_folder}/known_mps/catalog.csv", sep=';', index=False)
 	#df.to_csv("corpus/quality_assessment/known_mps/TEST_integrity-error_missing-birthdate.csv", sep=';', index=False)
 
 
 
 if __name__ == '__main__':
-	main()
+	import argparse
+	parser = argparse.ArgumentParser(description=__doc__)
+	parser.add_argument("--qa_folder", type=str, default="corpus/quality_assessment")
+	args = parser.parse_args()
+	main(args)
