9583c7d203f6322926566bb766dc793fc28e26ea
ninpnin <vainoyrjanainen@icloud.com>
2024-02-27 14:54:01 +0100

    refactor: use argparse

--- src/add-catalog-dob.py
+++ src/add-catalog-dob.py
@@ -3,9 +3,9 @@ from tqdm import tqdm
 import pandas as pd
 
 
-def main():
-	person = pd.read_csv("corpus/metadata/person.csv")
-	catalog = pd.read_csv("corpus/quality_assessment/known_mps/catalog.csv", sep=';')
+def main(args):
+	person = pd.read_csv(f"{args.metadata_folder}/person.csv")
+	catalog = pd.read_csv(f"{args.qa_folder}/known_mps/catalog.csv", sep=';')
 	DOBs = []
 	for i, r in tqdm(catalog.iterrows(), total=len(catalog)):
 		person_dobs = person[person['wiki_id'] == r['wiki_id']]
@@ -18,7 +18,13 @@ def main():
 
 	catalog['born'] = DOBs
 
-	catalog.to_csv("corpus/quality_assessment/known_mps/catalog.csv", sep=';')
+	catalog.to_csv(args.outpath, sep=';')
 
 if __name__ == '__main__':
-	main()
+	import argparse
+	argparser = argparse.ArgumentParser(description=__doc__)
+	argparser.add_argument("--metadata_folder", type=str, default="corpus/metadata")
+	argparser.add_argument("--qa_folder", type=str, default="corpus/quality_assessment")
+	argparser.add_argument("--outpath", type=str, default="corpus/quality_assessment/known_mps/catalog.csv")
+	args = argparser.parse_args()
+	main(args)
