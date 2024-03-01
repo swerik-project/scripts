d1d946001222261643c37e2ab1c7792a1a2c8ac3
Bob Borges <robert.d.borges@gmail.com>
2024-03-01 15:43:19 +0100

    feat: single script to add ID to u, seg, note and div

--- src/add_uuid.py
+++ src/add_uuid.py
@@ -2,6 +2,7 @@
 Add a randomly generated UUID to all elements in the XML ID field that are currently missing one.
 """
 from lxml import etree
+from pathlib import Path
 from pyriksdagen.utils import (
     elem_iter,
     get_formatted_uuid,
@@ -18,11 +19,25 @@ import multiprocessing
 
 
 def add_protocol_id(protocol):
-
+    print("add IDs")
     ids = set()
     num_ids = 0
 
     root, ns = parse_protocol(protocol, get_ns=True)
+
+    body = root.find(f".//{ns['tei_ns']}body")
+    if body is None:
+        print(protocol)
+    else:
+        divs = body.findall(f"{ns['tei_ns']}div")
+        for div in divs:
+            protocol_id = Path(protocol).stem
+            seed_str = f"{protocol_id}\n{' '.join(div.itertext())}"
+            x = div.attrib.get(f"{ns['xml_ns']}id", get_formatted_uuid(seed_str))
+            div.attrib[f"{ns['xml_ns']}id"] = x
+            num_ids += 1
+            ids.add(x)
+
     for tag, elem in elem_iter(root):
         if tag == "u":
             for subelem in elem:
@@ -56,9 +71,12 @@ def main(args):
     if args.protocol:
         protocols = [args.protocol]
     else:
-        protocols = sorted(list(protocol_iterators(
-                                        get_data_location("records"),
-                                        start=args.start, end=args.end)))
+        if args.records_folder is not None:
+            data_location = args.records_folder
+        else:
+            protocols = sorted(list(protocol_iterators(
+                                            get_data_location("records"),
+                                            start=args.start, end=args.end)))
 
     with multiprocessing.Pool() as pool:
         for i, n in tqdm(pool.imap(add_protocol_id, protocols), total=len(protocols)):
@@ -75,6 +93,10 @@ if __name__ == "__main__":
     parser.add_argument("--records_folder", type=str, default="corpus/records")
     parser.add_argument("-s", "--start", type=int, default=1920, help="Start year")
     parser.add_argument("-e", "--end", type=int, default=2022, help="End year")
+    parser.add_argument("-r", "--records-folder",
+                        type=str,
+                        default=None,
+                        help="(optional) Path to records folder, defaults to environment var or `data/`")
     parser.add_argument("-p", "--protocol",
                         type=str,
                         default=None,
