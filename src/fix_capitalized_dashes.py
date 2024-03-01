c65c4e51ac3f2f125b3a52b4f7bb0a3f2abd001a
Bob Borges <robert.d.borges@gmail.com>
2024-03-01 14:26:46 +0100

    refactor: operate on single protocol, get prot path from pyriksdagen.utils, rm outrrrraaageous package imports

--- src/fix_capitalized_dashes.py
+++ src/fix_capitalized_dashes.py
@@ -1,48 +1,58 @@
 """
 Concatenate split names of format "PERS- SON" into "PERSSON"
 """
-from lxml import etree
-import pandas as pd
-import os, progressbar, re
-import argparse
 from pyparlaclarin.read import paragraph_iterator
-from pyparlaclarin.refine import format_texts
-from pyriksdagen.utils import protocol_iterators
+from pyriksdagen.utils import (
+    get_data_location,
+    parse_protocol,
+    protocol_iterators,
+    write_protocol,
+)
 from tqdm import tqdm
+import argparse, re
+
+
 
 
 def main(args):
+    # NB: [A-ZÀ-Þ] is UPPERCASE LETTERS + Accented UPPERCASE letters, ÅÄÖ etc
     pattern = "([A-ZÀ-Þ]{2,10})(- )([A-ZÀ-Þ]{2,10})"
     e = re.compile(pattern)
 
-    protocols = sorted(list(protocol_iterators("corpus/protocols/", start=args.start, end=args.end)))
-    #print(protocols)
-    parser = etree.XMLParser(remove_blank_text=True)
+    if args.protocol:
+        protocols = [args.protocol]
+    else:
+        if args.records_folder is not None:
+            data_location = args.records_folder
+        else:
+            data_location = get_data_location("records")
+        protocols = sorted(list(protocol_iterators(data_location,
+                                                    start=args.start,
+                                                    end=args.end)))
 
     for protocol in tqdm(protocols, total=len(protocols)):
-        with open(protocol) as f:
-            root = etree.parse(f, parser).getroot()
+        root = parse_protocol(protocol)
+        for elem in paragraph_iterator(root, output="lxml"):
+            txt = elem.text
+            if txt is not None and len(e.findall(txt)) > 0:
+                elem.text = re.sub(pattern, r"\1\3", txt)
 
-            for elem in paragraph_iterator(root, output="lxml"):
-                pass  # if elem.text is not None:
-                #    print(elem.text)
-                txt = elem.text
-                if txt is not None and len(e.findall(txt)) > 0:
-                    elem.text = re.sub(pattern, r"\1\3", txt)
-                # e.match(string)
+        write_protocol(root, protocol)
 
-            root = format_texts(root)
 
-            b = etree.tostring(
-                root, pretty_print=True, encoding="utf-8", xml_declaration=True
-            )
 
-            with open(protocol, "wb") as f:
-                f.write(b)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("-s", "--start", type=int, default=1920, help="Start year")
     parser.add_argument("-e", "--end", type=int, default=2022, help="End year")
+    parser.add_argument("-r", "--records-folder",
+                        type=str,
+                        default=None,
+                        help="(optional) Path to records folder, defaults to environment var or `data/`")
+    parser.add_argument("-p", "--protocol",
+                        type=str,
+                        default=None,
+                        help="operate on a single protocol")
     args = parser.parse_args()
     main(args)
