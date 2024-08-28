4d8ab4c18d61a918976042f7b62b8d3b7ea836fc
ninpnin <vainoyrjanainen@icloud.com>
2024-08-28 12:54:36 +0200

    refactor: remove unnecessary arguments and imports

--- src/merge_utterances.py
+++ src/merge_utterances.py
@@ -1,16 +1,10 @@
 """
 Map introductions to the speaker in the metadata.
 """
-import pandas as pd
 from lxml import etree
 import argparse
-from pyriksdagen.db import load_metadata
-from pyriksdagen.refine import redetect_protocol
 from pyriksdagen.utils import protocol_iterators
-from pyriksdagen.segmentation import join_text
 from tqdm import tqdm
-from multiprocessing import Pool
-from functools import partial
 from pyriksdagen.utils import TEI_NS
 
 def merge_us(root):
@@ -46,10 +40,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--records_folder", type=str, default="corpus/records")
-    parser.add_argument("--metadata_root", type=str, default=".")
     parser.add_argument("-s", "--start", type=int, default=1867, help="Start year")
     parser.add_argument("-e", "--end", type=int, default=2022, help="End year")
-    parser.add_argument("--parallel", type=int, default=1, help="N parallel processes (default=1)")
-    parser.add_argument("--outfile", type=str, default="input/matching/unknowns.csv")
     args = parser.parse_args()
     main(args)
