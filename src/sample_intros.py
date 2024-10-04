d5936c417e26a68ac297c4c91c05a1ff2393ad90
ninpnin <vainoyrjanainen@icloud.com>
2024-10-04 11:23:33 +0200

    feat: add a flag to only sample unknown intros

--- src/sample_intros.py
+++ src/sample_intros.py
@@ -5,12 +5,13 @@ from pyriksdagen.utils import protocol_iterators
 import pandas as pd
 from lxml import etree
 from pyriksdagen.utils import elem_iter
+from pyriksdagen.refine import intro_to_dict
 import os
 import datetime
 from multiprocessing import Pool
 from tqdm import tqdm
 import argparse
-
+import warnings
 def extract_intros(protocol):
 	parser = etree.XMLParser(remove_blank_text=True)
 	root = etree.parse(protocol, parser).getroot()
@@ -39,7 +40,11 @@ def sample(df, n=1, by='year', random_state=None):
 	data = []
 	for strata in stratas:
 		df_strata = df[df[by] == strata]
-		data.append(df_strata.sample(n, random_state=random_state))
+		if len(df_strata) < n:
+			warnings.warn(f"Less than n ({len(df_strata)} vs {n}) instances to sample from")
+			data.append(df_strata)
+		else:
+			data.append(df_strata.sample(n, random_state=random_state))
 	return pd.concat(data).reset_index(drop=True)
 
 
@@ -52,11 +57,25 @@ def main(args):
 		for intros in tqdm(pool.imap(extract_intros, protocols), total=len(protocols)):
 			data.append(intros)
 	df = pd.concat(data).reset_index(drop=True)
-
+	print("Len df", len(df))
 	# Stratified sampling by year
 	df['year'] = df['github'].str.extract(r'(?:\/)(\d{4,8})(?:\/)')
 	df['year'] = df['year'].str[:4].astype(int)
+	if args.unknown:
+		df["indicator"] = 1
+		df = df[df["who"] == "unknown"]
 	df = sample(df, n=args.n, random_state=args.seed)
+	print("Len", len(df))
+	metadata = [intro_to_dict(intro_text) for intro_text in df["intro"]]
+	df["name"] = [d.get("name", "unknown") for d in metadata]
+	df["party"] = [d.get("party", "unknown") for d in metadata]
+	df["specifier"] = [d.get("specifier", "unknown") for d in metadata]
+	df["other"] = [d.get("other", "unknown") for d in metadata]
+	if args.unknown:
+		df = df.groupby(["name", "specifier", "other"]).sum()
+		df = df.sort_values("indicator")
+		df = df[df["indicator"] > 1].reset_index()
+	print(df[["intro", "name", "specifier", "indicator", "other"]])
 	df.to_csv(f"{args.outfolder}/intro_sample_{str(datetime.date.today())}.csv", index=False)
 
 
@@ -67,6 +86,7 @@ if __name__ == "__main__":
     parser.add_argument("--end", type=int, default=2022)
     parser.add_argument("--seed", type=int, default=None)
     parser.add_argument("--n", type=int, default=5)
+    parser.add_argument("--unknown", type=bool, default=False)
     parser.add_argument("--outfolder", type=str, default="input/accuracy")
     args = parser.parse_args()
     main(args)
