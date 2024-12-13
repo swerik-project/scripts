6361dce55d5931fecf12497cd64649dc00f27285
Bob Borges <robert.d.borges@gmail.com>
2024-12-13 17:31:53 +0100

    feat: add flag for skip sicknotes

--- src/cur-prot/find_dates.py
+++ src/cur-prot/find_dates.py
@@ -23,7 +23,7 @@ def main(args):
     for record in tqdm(args.records):
         metadata = infer_metadata(record)
         root, ns = parse_tei(record)
-        root, dates = detect_date(root, metadata)
+        root, dates = detect_date(root, metadata, skip_doctors_notes=args.skip_doctors_notes)
         write_tei(root, record)
 
 
@@ -31,4 +31,5 @@ def main(args):
 
 if __name__ == "__main__":
     parser = fetch_parser("records")
+    parser.add_argument("--skip-doctors-notes", action='store_true')
     main(impute_args(parser.parse_args()))
