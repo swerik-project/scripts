8f92233ea384b0f3e4eb32f0a1390cd67bb0a2cb
ninpnin <vainoyrjanainen@icloud.com>
2025-04-09 16:13:39 +0200

    feat: fix a common segmentation error: margin notes ending in '(Forts.)' are merged into the text body

--- src/cur-prot/split_forts.py
+++ src/cur-prot/split_forts.py
@@ -25,7 +25,7 @@ def split_text(t, pattern):
     p1, p2 = pattern.split()
     return s[0] + p1, p2 + s[1]
 
-def find_forts(root):
+def forts_herr(root):
     #re.compile("\\(Forts.\\) Herr [] \\:")
     pattern = "(Forts.) Herr"
     for tag, elem in elem_iter(root):
@@ -53,20 +53,54 @@ def find_forts(root):
     return root
 
 
+def ang_forts(root):
+
+    exp = re.compile(r"Ang\. [\w ]{10,40}(.) \((f|F)orts(\.)\) ")
+    #re.compile("\\(Forts.\\) Herr [] \\:")
+    
+    for tag, elem in elem_iter(root):
+        if tag == "u":
+            for seg in elem:
+                if seg.text is not None:
+                    elemtext = " ".join(seg.text.split())
+                    if exp.match(elemtext) is not None:
+                        matched_str = exp.match(elemtext).group(0)
+                        if len(matched_str) * 1.05 < len(elemtext.strip()):
+
+                            if elemtext.strip()[:4] == "Ang.":
+                                elemtext = elemtext.replace(matched_str, "")
+                                seg.text = matched_str
+                                newchild = etree.Element(f"{TEI_NS}seg")
+                                newchild.text = elemtext
+                                seg.addnext(newchild)
+        elif tag == "note":
+            if elem.text is not None:
+                elemtext = " ".join(elem.text.split())
+                if exp.match(elemtext) is not None:
+                    matched_str = exp.match(elemtext).group(0)
+                    if len(matched_str) * 1.05 < len(elemtext.strip()):
+
+                        if elemtext.strip()[:4] == "Ang.":
+                            elemtext = elemtext.replace(matched_str, "")
+                            elem.text = matched_str
+                            newchild = etree.Element(f"{TEI_NS}note")
+                            newchild.text = elemtext
+                            elem.addnext(newchild)
+
+    return root
+
 
 def main(args):
-    print(args.records)
     for protocol in tqdm(args.records):
         root, _ = parse_tei(protocol)
-        print(root)
-        root = find_forts(root)
+        if args.pattern == "forts-herr":
+            root = forts_herr(root)
+        else:
+            root = ang_forts(root)
         write_protocol(root, protocol)
-
-
 
 
 if __name__ == "__main__":
     parser = fetch_parser("records")
-    parser.add_argument("--json_path", type=str, default=[], nargs="+")
-    parser.add_argument("--skip-doctors-notes", action='store_true')
+    parser.add_argument("--pattern", type=str, default="forts-herr", choices=['forts-herr', 'ang-forts'])
     main(impute_args(parser.parse_args()))
