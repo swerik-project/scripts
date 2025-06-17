7e57f20e63db658e7801995b9cf02a5c8f8a8e8d
Bob Borges <robert.d.borges@gmail.com>
2025-06-17 16:53:39 +0200

    feat: print optd programatically

--- src/utils/r+w.py
+++ src/utils/r+w.py
@@ -8,6 +8,7 @@ from pyriksdagen.args import (
     interpellation_parser,
     motion_parser,
     record_parser,
+    volg_parser,
 )
 from pyriksdagen.io import (
     parse_tei,
@@ -28,22 +29,24 @@ def main(args):
 
 
 if __name__ == '__main__':
-    def _errmahgerd(err):
+
+    def _errmahgerd(err, corpora):
         errs = {
                 1: "You didn't set any first argument.",
                 2: "You passed an invalid doctype."
             }
-        raise ValueError(f"You have to pass the document type as the first argument of this script (--records, --motions, --interpellations)\n{errs[err]}")
+        raise ValueError(f"You have to pass the document type as the first argument of this script ({' '.join(['--'+k+',' for k in corpora.keys()])})\n{errs[err]}")
 
     corpora = {
             "records": record_parser,
             "motions": motion_parser,
             "interpellations": interpellation_parser,
+            "volg": volg_parser,
         }
     if not sys.argv[1]:
-        _errmahgerd(1)
+        _errmahgerd(1, corpora)
     if sys.argv[1][2:] in corpora:
         parser = fetch_parser(sys.argv[1][2:], docstring=__doc__)
         main(impute_args(parser.parse_args(sys.argv[2:])))
     else:
-        _errmahgerd(2)
+        _errmahgerd(2, corpora)
