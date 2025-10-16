c4c4cb0b247973b1bf4a9be241bd284fbb2ae68c
ninpnin <vainoyrjanainen@icloud.com>
2025-10-16 15:45:27 +0200

    refactor: add comments, remove unnecessary lines, add docstring

--- src/motion_sigblock_split.py
+++ src/motion_sigblock_split.py
@@ -1,10 +1,12 @@
+"""
+Splits the signature block of a motion with multiple names
+into multiple items. Currently optimized for 1950–1968ish
+"""
 from lxml import etree
 import argparse
-from pyriksdagen.utils import elem_iter, protocol_iterators, get_formatted_uuid
 from pyriksdagen.utils import TEI_NS, XML_NS
 from pyriksdagen.io import parse_tei, write_tei
 import tqdm 
-import multiprocessing
 from pyriksdagen.args import (
     fetch_parser,
     impute_args,
@@ -13,11 +15,11 @@ import polars as pl
 import string
 
 def process_signature_block(text):
-    text = text.replace(" i ", " i_")
-    text = text.split()
+    text = text.replace(" i ", " i_").split()
     labels = [wd in surnames or "i_" in wd or is_party_abbrev(wd) for wd in text]
     next_lables = labels[1:] + [False]
 
+    # Exclude cases with subsequent matches
     filtered_labels = [a and not b for a,b in zip(labels, next_lables)]
 
     # Process into a string with names delimited by a linebreak
@@ -35,21 +37,23 @@ def is_signature_heuristic(text):
     name_matches = [name in surnames for name in text.split()]
     return sum(name_matches) >= 1
 
-def process_motion(protocol):
-    root, _ = parse_tei(protocol, get_ns=True)
-
+def process_motion(motion_path):
+    root, _ = parse_tei(motion_path, get_ns=True)
     for body in root.findall(f".//{TEI_NS}body"):
         for div in body.findall(f".//{TEI_NS}div"):
             if div.attrib.get("type") == "signatureBlock":
                 for l in div.findall(f".//{TEI_NS}list"):
+
+                    # Only process if multiple names found
                     if len(list(l)) >= 2:
-                        
                         for elem in list(l):
                             text = "\n".join(elem.itertext())
                             text, last_is_surname = process_signature_block(text)
                             current = elem
 
+                            # Only process signatures that haven't been matched to the MP database
                             if len(text) >= 2 and elem.attrib.get("who", "unknown") == "unknown":
+
                                 # If the last name is not a surname, concatenate it to the next element
                                 if not last_is_surname and l.index(elem) < len(l) - 1:
                                     last_name_in_list = text[-1]
@@ -57,19 +61,16 @@ def process_motion(protocol):
                                     next_elem = l[l.index(elem)+1]
                                     next_elem.text = last_name_in_list + " " + next_elem.text
 
+                                # Add new elements for the signatures
                                 for t in text:
                                     p = etree.Element(f"{TEI_NS}item")
                                     p.text = t
                                     if is_signature_heuristic(t):
                                         p.attrib["type"] = "signature"
-                                    print(t, is_signature_heuristic(t))
                                     l.insert(l.index(current)+1, p)
                                     current = p
-                                
                                 l.remove(elem)
-                            print(text)
-
-    write_tei(root, protocol)
+    write_tei(root, motion_path)
 
 
 def main(args):
@@ -78,7 +79,6 @@ def main(args):
 
     for motion in tqdm.tqdm(args.motions):
         process_motion(motion, surnames)
-
 
 if __name__ == "__main__":
     parser = fetch_parser("motions")
