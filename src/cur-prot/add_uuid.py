5aed6c2e49dba2aa30819a3985a4eb6865cf7a37
mandlilaast <erik.mandel.4390@student.uu.se>
2026-01-19 11:20:40 +0100

    Fix: make add_uuid.py generate deterministic and reproducible ID-s

--- src/cur-prot/add_uuid.py
+++ src/cur-prot/add_uuid.py
@@ -3,82 +3,114 @@ Add a randomly generated UUID to all elements in the XML ID attribute that are c
 
 Also adds the document ID (eg. prot-year--number) in the TEI element as an XML ID attribute if its missing.
 """
-from lxml import etree
-import argparse
-from pyriksdagen.utils import elem_iter, protocol_iterators, get_formatted_uuid
-from pyriksdagen.utils import TEI_NS, XML_NS
-from tqdm import tqdm
-import multiprocessing
+from collections import Counter
 from pyriksdagen.args import (
     fetch_parser,
     impute_args,
 )
+from pyriksdagen.io import (
+    parse_tei,
+    write_tei
+)
+from pyriksdagen.utils import (
+    elem_iter,
+    get_formatted_uuid
+)
+from pyriksdagen.utils import (
+        TEI_NS,
+        XML_NS
+)
+from tqdm import tqdm
+import multiprocessing
 
 def add_protocol_id(protocol):
     ids = set()
-    root, _ = parse_protocol(protocol, get_ns=True)
-    
-    # Accomodate both TEI and teiCorpus root
+    seed_map = {}
+    root, ns = parse_tei(protocol)
+
     tei = root.find(f"{TEI_NS}TEI")
     if root.tag.split("}")[-1] == "TEI":
         tei = root
 
-    # Set ID for TEI element to be the filename
+    doc_id = protocol.split("/")[-1][:-4]
     if f"{XML_NS}id" not in tei.attrib:
-        tei.attrib[f"{XML_NS}id"] = protocol.split("/")[-1][:-4]
+        tei.attrib[f"{XML_NS}id"] = doc_id
 
-    # Create UUIDs for other elements
     num_ids = 0
+    note_counter = 0
+    u_counter = 0
     for tag, elem in elem_iter(root):
         if tag == "u":
-            for subelem in elem:
-                x = subelem.attrib.get(f'{XML_NS}id', get_formatted_uuid())
-                subelem.attrib[f'{XML_NS}id'] = x
-                ids.add(x)
+            for idx, subelem in enumerate(elem):
+                if f'{XML_NS}id' not in subelem.attrib:
+                    seed_str = f"{doc_id}\n{tag}_sub\n{idx}"
+                    id_val = get_formatted_uuid(seed_str)
+                    subelem.attrib[f'{XML_NS}id'] = id_val
+                    seed_map[id_val] = seed_str
+                ids.add(subelem.attrib[f'{XML_NS}id'])
                 num_ids += 1
-            x = elem.attrib.get(f'{XML_NS}id', get_formatted_uuid())
-            elem.attrib[f'{XML_NS}id'] = x
-            ids.add(x)
-            num_ids += 1
-                
-        elif tag in ["note"]:
-            x = elem.attrib.get(f'{XML_NS}id', get_formatted_uuid())
-            elem.attrib[f'{XML_NS}id'] = x
-            ids.add(x)
-            num_ids += 1
 
-    # Add IDs for divs
+            if f'{XML_NS}id' not in elem.attrib:
+                seed_str = f"{doc_id}\n{tag}\n{u_counter}"
+                id_val = get_formatted_uuid(seed_str)
+                elem.attrib[f'{XML_NS}id'] = id_val
+                seed_map[id_val] = seed_str
+            ids.add(elem.attrib[f'{XML_NS}id'])
+            num_ids += 1
+            u_counter += 1
+
+        elif tag == "note":
+            if f'{XML_NS}id' not in elem.attrib:
+                seed_str = f"{doc_id}\n{tag}\n{note_counter}"
+                id_val = get_formatted_uuid(seed_str)
+                elem.attrib[f'{XML_NS}id'] = id_val
+                seed_map[id_val] = seed_str
+            ids.add(elem.attrib[f'{XML_NS}id'])
+            num_ids += 1
+            note_counter += 1
+
     for body in root.findall(f".//{TEI_NS}body"):
+        div_counter = 0
         for div in body:
-            elem_id_list = [elem.attrib.get(f'{XML_NS}id') for elem in div]
-            elem_id_list = [elem_id for elem_id in elem_id_list if elem_id is not None]
-            elem_id_list = '\n'.join(elem_id_list)
-            seed_str =  f"div\n{elem_id_list}"
-            new_div_id = get_formatted_uuid(seed_str)
             if f'{XML_NS}id' not in div.attrib:
-                print(seed_str, new_div_id)
-            x = div.get(f'{XML_NS}id', new_div_id)
-            div.attrib[f'{XML_NS}id'] = x
-            ids.add(x)
+                seed_str = f"{doc_id}\ndiv\n{div_counter}"
+                id_val = get_formatted_uuid(seed_str)
+                div.attrib[f'{XML_NS}id'] = id_val
+                seed_map[id_val] = seed_str
+            ids.add(div.attrib[f'{XML_NS}id'])
             num_ids += 1
+            div_counter += 1
 
-    write_protocol(root, protocol)
+    write_tei(root, protocol)
 
-    assert len(ids) == num_ids
-    return ids, num_ids
+    return ids, num_ids, seed_map
 
 
 def main(args):
     protocols = args.records
     num_ids = 0
     ids = []
+
+    all_seed_maps = {}
     with multiprocessing.Pool() as pool:
-        for i, n in tqdm(pool.imap(add_protocol_id, protocols), total=len(protocols)):
+        for i, n, smap in tqdm(pool.imap(add_protocol_id, protocols), total=len(protocols)):
             ids += i
             num_ids += n
+            all_seed_maps.update(smap)
 
-        assert len(set(ids)) == num_ids
+    c = Counter(ids)
+    duplicates = {id_val: count for id_val, count in c.items() if count > 1}
 
+    if duplicates:
+        print("Some duplicate IDs found:")
+        for id_val, count in list(duplicates.items())[:10]:
+            print(f"{id_val} (used {count} times), seed: {all_seed_maps.get(id_val, 'N/A')}")
+
+    print(f"Total IDs generated: {num_ids}")
+    print(f"Unique IDs: {len(set(ids))}")
+    print(f"Duplicate IDs: {len(duplicates)}")
+
+    assert len(set(ids)) == num_ids, "There are duplicate IDs!"
 
 if __name__ == "__main__":
     parser = fetch_parser("records")
