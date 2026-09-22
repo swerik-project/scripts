1c9e9702284ef75f9ee9b2668ebc4e9a4dbbfec8
ninpnin <vainoyrjanainen@icloud.com>
2026-09-22 14:49:00 +0200

    fix: more accurate signature splitting

--- src/cur-mot/split-multiname-signatures.py
+++ src/cur-mot/split-multiname-signatures.py
@@ -16,7 +16,9 @@ from pyriksdagen.io import (
 from pyriksdagen.utils import (
     get_data_location,
     infer_metadata,
-    first_and_last_names
+    first_and_last_names,
+    get_formatted_uuid,
+    XML_NS
 )
 from tqdm import tqdm
 import polars as pl
@@ -29,28 +31,67 @@ def split_signature(elem, first_names, last_names, iort):
     t = ' '.join(elem.text.split())
     t_before = t
     words = t.split()
+
+    new_parts = []
+    current_part = [words[0]]
     for w0, w1 in zip(words[:-1], words[1:]):
         if "i" not in [w0,w1]:
-            cond1 = w0 in last_names.union(iort)
-            cond1 = cond1 or (w0[0].isupper() and "son" == w0[-3:])
-            cond2 = w1 in first_names or (w1 not in last_names and w1[0].isupper())
+            w0_clean = w0.replace(".", "")
+            w1_clean = w1.replace(".", "")
+            cond1 = w0_clean in last_names.union(iort)
+            cond1 = cond1 or (w0_clean[0].isupper() and "son" == w0_clean[-3:])
+            cond2 = w1_clean in first_names or (w1_clean not in last_names and w1_clean[0].isupper())
 
             if cond1 and cond2:
                 LOGGER.debug(f"Split between : {w0} and {w1}")
                 t = t.replace(f"{w0} {w1}", f"{w0}; {w1}")
-    #exit()
+                new_parts.append(current_part)
+                current_part = []
+
+        current_part.append(w1)
+
+    if len(current_part) >= 1:
+        new_parts.append(current_part)
+
+    new_parts = [" ".join(p) for p in new_parts]
+
+    assert " ".join(new_parts) == t_before
+
+    signature_list = elem.getparent()    
+    original_id = elem.attrib[f"{XML_NS}id"]
+    original_ix = signature_list.index(elem)
     if t_before != t:
         LOGGER.info(f"{t_before} => {t}")
+        LOGGER.debug(f"{new_parts}")
 
-    return
+        elem.text = new_parts[0]
+        
+        for ix, signature_text in enumerate(new_parts[1:]):
+            item = etree.Element("item")
+            item.text = signature_text
+            id_seed = f"split_signature\n{original_id}\n{ix}"
+            item.attrib[f"{XML_NS}id"] = get_formatted_uuid(id_seed)
 
+            letter_count = len([ch for ch in signature_text if ch.isalpha()])
+            if letter_count >= 3:
+                item.attrib["who"] = "unknown"
+                item.attrib["type"] = "signature"
+            signature_list.insert(original_ix + ix + 1, item)
 
+        return len(new_parts) - 1
+
+    return 0
+
+#i-GVxPCrypLVRDCytv2cGoH3
 def main(args):
     metadata_location = get_data_location("metadata")
 
     df_names = pl.read_csv(f'{metadata_location}/name.csv')
     df_iort = pl.read_csv(f'{metadata_location}/location_specifier.csv')
     first_names, last_names, iort = first_and_last_names(df_names, df_iort)
+
+    # Ad-hoc first names
+    #first_names = first_names + {"Erik", "Johan"}
 
     with open("first_names.txt", "w") as f:
         f.write("\n".join(list(first_names)))
@@ -59,6 +100,7 @@ def main(args):
     with open("iort.txt", "w") as f:
         f.write("\n".join(list(iort)))
 
+    no_splits = 0
     # Pre-load regex for intro_to_dict for performance reasons
     for i, motion in enumerate(tqdm(sorted(args.motions))):
         py = motion.split("/")[2]
@@ -75,14 +117,15 @@ def main(args):
                 t = ' '.join(item.text.split())
                 if len(t) > 0:
                     if item.attrib.get("type") == "signature":
-                        multiple_names = [wd in last_names for wd in t.split()]
+                        multiple_names = [wd in last_names for wd in t.replace(".", "").split()]
                         multiple_names = sum(multiple_names) >= 2
                         if multiple_names:
                             #LOGGER.warning(f"Multiple names: {t}")
-                            split_signature(item, first_names, last_names, iort)
+                            no_splits += split_signature(item, first_names, last_names, iort)
 
-        #exit()
-        #write_tei(root, motion)
+        write_tei(root, motion)
+
+    LOGGER.train(f"In total, {no_splits} signature splits were made ({no_splits/len(args.motions)} per motion)")
     df = pd.DataFrame(lens, columns = ["motion", "length_of_sig_block", "sig_block_text"])
     df.to_csv("input/motion_sig_block_len.csv", index=False)
     #{print(k, v) for k, v in dict(sorted(lens_counts.items(), key=lambda item: item[1])).items()}
